c7abb26df5c8a82c1bccc3cd0d1bf3f6a1079a65
codewesleylima <gusterlima@hotmail.com>
2026-04-12 14:51:00 -0300

    feat(service): add new methodthat process a new registers in our databases

---
+++ src/main/java/com/wzzy/contrato/service/ContratoService.java
@@ -0,0 +1,9 @@
+package com.wzzy.contrato.service;
+
+import com.wzzy.contrato.model.ContratoModel;
+
+public interface ContratoService {
+
+    ContratoModel cadastrarContrato(ContratoModel contratoModel);
+
+}
