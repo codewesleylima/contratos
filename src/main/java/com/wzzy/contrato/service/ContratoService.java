105628eabe43daf0afef753fc4e07d527cecfd88
codewesleylima <gusterlima@hotmail.com>
2026-04-12 16:14:44 -0300

    refactor(service): refected method about list contracts for using name more relational with your function

--- src/main/java/com/wzzy/contrato/service/ContratoService.java
+++ src/main/java/com/wzzy/contrato/service/ContratoService.java
@@ -2,8 +2,11 @@ package com.wzzy.contrato.service;
 
 import com.wzzy.contrato.model.ContratoModel;
 
+import java.util.List;
+
 public interface ContratoService {
 
     ContratoModel cadastrarContrato(ContratoModel contratoModel);
 
+    List<ContratoModel> listarContratos();
 }
