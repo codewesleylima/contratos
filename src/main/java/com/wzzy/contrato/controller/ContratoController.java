45c2dbbf4c914b5d1a5e76172cdaae9097a4e48a
codewesleylima <gusterlima@hotmail.com>
2026-04-12 16:15:36 -0300

    feat(controller): build a new method for request a methos that get all contracts

--- src/main/java/com/wzzy/contrato/controller/ContratoController.java
+++ src/main/java/com/wzzy/contrato/controller/ContratoController.java
@@ -5,10 +5,9 @@ import com.wzzy.contrato.service.ContratoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/contratos")
@@ -28,5 +27,9 @@ public class ContratoController {
         return new ResponseEntity<>(novoContrato, HttpStatus.CREATED);
     }
 
+    @GetMapping("/listar-todos")
+    public List<ContratoModel> listarContratos() {
+        return contratoService.listarContratos();
+    }
 
 }
