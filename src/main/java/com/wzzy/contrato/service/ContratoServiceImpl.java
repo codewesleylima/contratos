2e8e728566f69b4a1ef686dac0c661ca487974de
codewesleylima <gusterlima@hotmail.com>
2026-04-12 14:55:06 -0300

    fix(serviceImpl): remove imports that not are necessary

--- src/main/java/com/wzzy/contrato/service/ContratoServiceImpl.java
+++ src/main/java/com/wzzy/contrato/service/ContratoServiceImpl.java
@@ -1,12 +1,8 @@
 package com.wzzy.contrato.service;
 
-import com.wzzy.contrato.model.Contrato;
 import com.wzzy.contrato.model.ContratoModel;
 import com.wzzy.contrato.repository.ContratoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class ContratoServiceImpl implements ContratoService {
