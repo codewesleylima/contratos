f1ecae7bec92b6a0eec0930216e91d052d57b552
codewesleylima <gusterlima@hotmail.com>
2026-04-12 14:53:12 -0300

    feat(model): build a new class for process field of the contracts

---
+++ src/main/java/com/wzzy/contrato/model/ContratoModel.java
@@ -0,0 +1,25 @@
+package com.wzzy.contrato.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contrato")
+public class ContratoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long contratoId;
+
+    private String cpf;
+    private String nomeTitular;
+    private double valorMensal;
+    private boolean ativo;
+}
