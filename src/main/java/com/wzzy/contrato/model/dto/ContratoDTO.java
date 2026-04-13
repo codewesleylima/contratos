4359d2096d41093770bde6640fb04ab76902e178
codewesleylima <gusterlima@hotmail.com>
2026-04-13 19:54:37 -0300

    feat(dto): create ContratoDTO record for contract registration

---
+++ src/main/java/com/wzzy/contrato/model/dto/ContratoDTO.java
@@ -0,0 +1,11 @@
+package com.wzzy.contrato.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ContratoDTO(
+        @NotBlank String cpf,
+        @NotBlank String nomeTitular,
+        @NotNull Double valorMensal,
+        @NotNull Boolean Status){
+}
