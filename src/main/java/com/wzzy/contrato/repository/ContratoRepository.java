9ec368a75702a8a404fbcc25ce55fe3f34de3ddb
codewesleylima <gusterlima@hotmail.com>
2026-04-12 22:06:36 -0300

    refactor(repository): fixed type method for find with name

--- src/main/java/com/wzzy/contrato/repository/ContratoRepository.java
+++ src/main/java/com/wzzy/contrato/repository/ContratoRepository.java
@@ -4,10 +4,11 @@ import com.wzzy.contrato.model.ContratoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ContratoRepository extends JpaRepository<ContratoModel, Long> {
 
     List<ContratoModel> findByStatus(boolean status);
 
-    List<ContratoModel> findByNomeTitular(String nomeTitular);
+    Optional<ContratoModel> findByNomeTitular(String nomeTitular);
 }
