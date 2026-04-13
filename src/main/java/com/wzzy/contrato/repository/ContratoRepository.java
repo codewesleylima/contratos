d9054704b6d65331958e891435610f6190154cdf
codewesleylima <gusterlima@hotmail.com>
2026-04-13 20:13:00 -0300

    refactor(repository): Change typo id for UUID

--- src/main/java/com/wzzy/contrato/repository/ContratoRepository.java
+++ src/main/java/com/wzzy/contrato/repository/ContratoRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface ContratoRepository extends JpaRepository<ContratoModel, Long> {
+public interface ContratoRepository extends JpaRepository<ContratoModel, UUID> {
 
     List<ContratoModel> findByStatus(boolean status);
 
