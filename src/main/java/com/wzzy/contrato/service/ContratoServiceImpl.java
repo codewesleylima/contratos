80096244e48f5d7925458a3290fca39709bdd484
codewesleylima <gusterlima@hotmail.com>
2026-04-13 23:41:30 -0300

    refactor(serviceImpl): update ContratoServiceImpl to handle DadosContratoDTO with related entities

--- src/main/java/com/wzzy/contrato/service/ContratoServiceImpl.java
+++ src/main/java/com/wzzy/contrato/service/ContratoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.wzzy.contrato.service;
 
 import com.wzzy.contrato.model.DadosContratoModel;
+import com.wzzy.contrato.model.DadosEnderecoModel;
+import com.wzzy.contrato.model.DadosPessoaisModel;
+import com.wzzy.contrato.model.DadosProfissionaisModel;
 import com.wzzy.contrato.model.dto.DadosContratoDTO;
 import com.wzzy.contrato.repository.ContratoRepository;
 import org.springframework.stereotype.Service;
@@ -20,27 +23,58 @@ public class ContratoServiceImpl implements ContratoService {
     @Override
     public DadosContratoModel cadastrarContrato(DadosContratoDTO dadosContratoDTO) {
         DadosContratoModel dadosContratoModel = new DadosContratoModel();
+        dadosContratoModel.setDadosPessoaisModel(new DadosPessoaisModel());
+        dadosContratoModel.setDadosProfissionais(new DadosProfissionaisModel());
+        dadosContratoModel.setDadosEnderecoModel(new DadosEnderecoModel());
 
-        dadosContratoModel.getDadosPessoaisModel().setCpf(dadosContratoDTO.cpf());
-        dadosContratoModel.getDadosPessoaisModel().setNomeCompleto(dadosContratoDTO.nomeTitular());
-        dadosContratoModel.setValorMensal(dadosContratoDTO.valorMensal());
-        dadosContratoModel.setStatus(dadosContratoDTO.status());
+        // Dados Pessoais
+        dadosContratoModel.getDadosPessoaisModel().setCpf(dadosContratoDTO.dadosPessoaisDTO().cpf());
+        dadosContratoModel.getDadosPessoaisModel().setNomeCompleto(dadosContratoDTO.dadosPessoaisDTO().nomeCompleto());
+        dadosContratoModel.getDadosPessoaisModel().setRg(dadosContratoDTO.dadosPessoaisDTO().rg());
+        dadosContratoModel.getDadosPessoaisModel().setDataNascimento(dadosContratoDTO.dadosPessoaisDTO().dataNascimento());
+        dadosContratoModel.getDadosPessoaisModel().setEstadoCivil(dadosContratoDTO.dadosPessoaisDTO().estadoCivil());
+        dadosContratoModel.getDadosPessoaisModel().setDataNascimento(dadosContratoDTO.dadosPessoaisDTO().nacionalidade());
+        dadosContratoModel.getDadosPessoaisModel().setNaturalidade(dadosContratoDTO.dadosPessoaisDTO().naturalidade());
+
+        // Dados Profissionais
+        dadosContratoModel.getDadosProfissionais().setProfissao(dadosContratoDTO.dadosProfissionaisDTO().profissao());
+        dadosContratoModel.getDadosProfissionais().setCargo(dadosContratoDTO.dadosProfissionaisDTO().cargo());
+        dadosContratoModel.getDadosProfissionais().setFuncao(dadosContratoDTO.dadosProfissionaisDTO().funcao());
+        dadosContratoModel.getDadosProfissionais().setSalario(dadosContratoDTO.dadosProfissionaisDTO().salario());
+        dadosContratoModel.getDadosProfissionais().setDepartamento(dadosContratoDTO.dadosProfissionaisDTO().departamento());
+        dadosContratoModel.getDadosProfissionais().setDataAdmissao(dadosContratoDTO.dadosProfissionaisDTO().dataAdmissao());
+        dadosContratoModel.getDadosProfissionais().setSalarioBruto(dadosContratoDTO.dadosProfissionaisDTO().salarioBruto());
+        dadosContratoModel.getDadosProfissionais().setSalarioLiquido(dadosContratoDTO.dadosProfissionaisDTO().salarioLiquido());
+        dadosContratoModel.getDadosProfissionais().setJornadaTrabalho(dadosContratoDTO.dadosProfissionaisDTO().jornadaTrabalho());
+        dadosContratoModel.getDadosProfissionais().setTipoContrato(dadosContratoDTO.dadosProfissionaisDTO().tipoContrato());
+
+        // Dados Endereco
+        dadosContratoModel.getDadosEnderecoModel().setRua(dadosContratoDTO.dadosEnderecoDTO().rua());
+        dadosContratoModel.getDadosEnderecoModel().setNumero(dadosContratoDTO.dadosEnderecoDTO().numero());
+        dadosContratoModel.getDadosEnderecoModel().setComplemento(dadosContratoDTO.dadosEnderecoDTO().complemento());
+        dadosContratoModel.getDadosEnderecoModel().setBairro(dadosContratoDTO.dadosEnderecoDTO().bairro());
+        dadosContratoModel.getDadosEnderecoModel().setCidade(dadosContratoDTO.dadosEnderecoDTO().cidade());
+        dadosContratoModel.getDadosEnderecoModel().setEstado(dadosContratoDTO.dadosEnderecoDTO().estado());
+        dadosContratoModel.getDadosEnderecoModel().setCep(dadosContratoDTO.dadosEnderecoDTO().cep());
+
 
         return repository.save(dadosContratoModel);
     }
 
-    @Override
-    public List<DadosContratoModel> buscarTodosContros() {
-        return repository.findAll();
-    }
-
-    @Override
-    public List<DadosContratoModel> buscarContratosPorCpf(String cpf) {
-        return repository.findByCpf(cpf);
-    }
-
-    @Override
-    public Optional<DadosContratoModel> buscarContratoPorNome(String nome) {
-        return repository.findByNomeTitular(nome);
-    }
+//    @Override
+//    public List<DadosContratoModel> buscarTodosContros() {
+//        return repository.findAll();
+//    }
+//
+//    @Override
+//    public List<DadosContratoModel> buscarContratosPorCpf(DadosContratoDTO dadosContratoDTO) {
+//
+//        DadosContratoModel dadosContratoModel = new DadosContratoModel(dadosContratoDTO().g)
+//        return repository.findDadosContratoModelByDadosPessoaisModel_Cpf(dadosContratoDTO);
+//    }
+//
+//    @Override
+//    public Optional<DadosContratoModel> buscarContratoPorNome(String nome) {
+//        return repository.findByNomeTitular(nome);
+//    }
 }
