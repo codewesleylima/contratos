90daea3abb6489509bcc13ee6da272f91733ab5a
codewesleylima <gusterlima@hotmail.com>
2026-04-13 21:11:40 -0300

    refactor(repository): rename references with class model

--- src/main/java/com/wzzy/contrato/repository/ContratoRepository.java
+++ src/main/java/com/wzzy/contrato/repository/ContratoRepository.java
@@ -1,15 +1,15 @@
 package com.wzzy.contrato.repository;
 
-import com.wzzy.contrato.model.ContratoModel;
+import com.wzzy.contrato.model.DadosContratoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ContratoRepository extends JpaRepository<ContratoModel, UUID> {
+public interface ContratoRepository extends JpaRepository<DadosContratoModel, UUID> {
 
-    List<ContratoModel> findByCpf(String cpf);
+    List<DadosContratoModel> findByCpf(String cpf);
 
-    Optional<ContratoModel> findByNomeTitular(String nomeTitular);
+    Optional<DadosContratoModel> findByNomeTitular(String nomeTitular);
 }
