a1b799448942d9471b4dabbe35ef683e792e3f00
codewesleylima <gusterlima@hotmail.com>
2026-04-13 21:10:11 -0300

    feat(model): build a new class for professional data

---
+++ src/main/java/com/wzzy/contrato/model/DadosProfissionaisModel.java
@@ -0,0 +1,36 @@
+package com.wzzy.contrato.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_DADOS_PROFISSIONAIS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DadosProfissionaisModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID dadosProfissionaisId;
+
+    private String profissao;
+    private String cargo;
+    private String funcao;
+    private String salario;
+    private String departamento;
+    private String dataAdmissao;
+    private String salarioBruto;
+    private String salarioLiquido;
+    private String jornadaTrabalho;
+    private String tipoContrato;
+
+
+}
