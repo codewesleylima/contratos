10c34ac05010e426de3f830f6d38f0e1069e1657
codewesleylima <gusterlima@hotmail.com>
2026-04-13 22:01:33 -0300

    fix(dto): fixed references about contract in this class

--- src/main/java/com/wzzy/contrato/model/dto/DadosContratoDTO.java
+++ src/main/java/com/wzzy/contrato/model/dto/DadosContratoDTO.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record DadosContratoDTO(
-        @NotBlank String cpf,
-        @NotBlank String nomeCompleto,
-        @NotNull double valorMensal,
-        @NotNull boolean status){
+
+        DadosPessoaisDTO dadosPessoaisDTO,
+        DadosProfissionaisDTO dadosProfissionaisDTO,
+        DadosEnderecoDTO dadosEnderecoDTO){
 }
